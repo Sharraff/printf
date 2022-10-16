a2ff118e404d358a836381b239e6d9cac0a4139a
daniell-olaitan <100495367+daniell-olaitan@users.noreply.github.com>
2022-10-16 13:22:12 +0100

    Update print_s.c

--- print_s.c
+++ print_s.c
@@ -10,10 +10,10 @@
 int print_s(va_list s)
 {
 	char *str = va_arg(s, char *);
-	int i;
+	int i = 0;
 
 	if (str == NULL)
-		str = "null";
+		str = "(null)";
 	while (str[i])
 	{
 	 	_putchar(str[i++]);
