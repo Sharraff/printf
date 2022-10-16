8b57e556d0511dd38fc66883e29360139b85db6f
daniel-olaitan <daniell.olaitan@gmail.com>
2022-10-15 23:36:48 -0700

    Updated main.h to contain the struct that match a specifier to the function that process them

--- main.h
+++ main.h
@@ -2,7 +2,15 @@
 #define PRINTF_H
 
 #include <unistd.h>
+#include <stdarg.h>
 
 int _putchar(char c);
+int print_c(va_list);
+
+typedef struct fmt
+{
+	char spec;
+	int (*func)(va_list);
+} fmt_spec;
 
 #endif
