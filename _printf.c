c26f311c11b662dbf8b584b5c892ab93a736b703
daniell-olaitan <100495367+daniell-olaitan@users.noreply.github.com>
2022-10-17 09:56:55 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -7,36 +7,17 @@
  *@fmt: list of arguments to printing
  *Return: Number of characters to printing
  */
-int _printf(char *fmt, ...)
+int _printf(const char *format, ...)
 {
 	va_list ap;
-	int (*f)(va_list);
-	unsigned int i = 0;
-	unsigned int counter = 0;
+	int counter = 0;
 
-	if (fmt == NULL)
+	if (format == NULL)
 		return (-1);
-	va_list (ap, fmt);
-	while (fmt && fmt[i])
-	{
-		if (fmt[i] != '%')
-		{
-			_putchar(fmt[i]);
-			counter++;
-			i++;
-			continue;
-		}
-		else
-		{
-			f = print_format(&fmt[i + 1]);
-			if (f = NULL)
-				return (-1);
-			i += 2;
-			counter += f(ap);
-			continue;
-		}
-		i++;
-	}
+	
+	va_start(ap, format);
+	counter = print_format(format, ap);
 	va_end(ap);
+	
 	return (counter);
 }
