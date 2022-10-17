598999882db1714c48eab3d786e8a1c957903bbf
root <root@DESKTOP-67TLRHA.localdomain>
2022-10-17 11:58:39 -0700

    added print_X function prototype to main.h

--- main.h
+++ main.h
@@ -13,6 +13,7 @@ int print_format(const char *format, va_list args);
 int _printf(const char *format, ...);
 int print_b(va_list b);
 int print_x(va_list x);
+int print_X(va_list X);
 
 /**
  * struct fmt - struct to match a specifier to a function that processes it
