d1c7ac66c19ce95624dcd66f78d2e89cc1cc4479
daniel-olaitan <daniell.olaitan@gmail.com>
2022-10-20 06:47:39 -0700

    Debugging

--- print_int.c
+++ print_int.c
@@ -17,7 +17,7 @@ int print_int(va_list integer, flag *f)
 	if (f->plus && num >= 0)
 		length += _putchar('+');
 
-	if (f->space)
+	if (f->space && n >= 0)
 		length += _putchar(' ');
 
 	if (num < 0)
